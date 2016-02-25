0f3483a3d69f20460050df68f10b3f799a42035f
Thomas Huybrechts <huybthomas@gmail.com>
2016-02-25 11:58:18 +0100

    Initial commit

---
+++ SimCity Viewer/src/main/java/be/uantwerpen/sc/SimCityViewerApplication.java
@@ -0,0 +1,12 @@
+package be.uantwerpen.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimCityViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimCityViewerApplication.class, args);
+	}
+}
---
+++ SimCity Worker/src/test/java/be/uantwerpen/sc/SimCityWorkerApplicationTests.java
@@ -0,0 +1,16 @@
+package be.uantwerpen.sc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SimCityWorkerApplication.class)
+public class SimCityWorkerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
